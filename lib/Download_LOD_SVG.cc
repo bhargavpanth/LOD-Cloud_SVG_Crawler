2e85dbbf4e992b30b09d8256ded22438b2173ed4
Bhargav Panth <bhargavrpanth@gmail.com>
2018-07-13 15:33:30 +0100

    first commit

---
+++ lib/Download_LOD_SVG.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+
+/**
+ * This class takes a URL and downloads the SVG to cache folder in the root
+ * **/
---
+++ run.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<curl/curl.h>
+
+/*
+int main(int argc, char **argv){
+	std::cout<<"This is a print statement";
+	return 0;
+}
+*/
+
+
